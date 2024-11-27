3b8c63cc844cfb329e37b639137ec1f3f1e18b8a
Nisha <rainishani400@gmail.com>
2024-11-27 18:48:32 +0545

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+
+ import Title from "./Title.jsx";
+function Description(){
+  return <h3>i am Description</h3>;
+
+}
+function App() {
+  
+
+  return ( <div>
+    <Title />
+     <Description />
+  
+    
+    <Title/>
+    <Description />
+  </div>
+  );
+}
+
+export default App;
---
+++ src/Title.jsx
@@ -0,0 +1,10 @@
+function Title(){
+    let name ="Nisha";
+    return  (
+        <div>
+            <p>2*2 ={ 2* 2}</p>
+            <p> Hi ,{ name.toUpperCase()}</p>
+        </div>
+    );
+   }
+   export default Title;
