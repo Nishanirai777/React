d5bd901535859ea48cf76756f353fb5dd740b709
Nisha <rainishani400@gmail.com>
2024-12-07 15:02:59 +0545

    Add files via upload

---
+++ day3/Pproduct.jsx
@@ -0,0 +1,13 @@
+import "./Pproduct.css";
+function Pproduct ( {title ,price ,features }){
+
+   return(
+ <div className="Pproduct">
+    <h1>{title}</h1>
+    <h5> price : {price}</h5>
+    <p>{features.map((feature) =>  <li>{feature}</li>)}</p>
+   
+ </div>
+);
+}
+export default Pproduct;
