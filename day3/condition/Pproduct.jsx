209a312682dc43f6bd5d53bb77cc665f07fa8437
Nisha <rainishani400@gmail.com>
2024-12-07 15:30:19 +0545

    Add files via upload

--- day3/condition/Pproduct.jsx
+++ day3/condition/Pproduct.jsx
@@ -1,25 +1,15 @@
 import "./Pproduct.css";
 function Pproduct ( {title ,price ,features }){
- if(price>30000){
+ 
 
    return(
       <div className="Pproduct">
          <h1>{title}</h1>
          <h5> price : {price}</h5>
-         <p>Discount 5%</p>
+        { price>30000? <p>"discount of 5%"</p>: <a href="/get discount"></a>}
         
       </div>
      );
- } else{
-   
-   return(
-      <div className="Pproduct">
-         <h1>{title}</h1>
-         <h5> price : {price}</h5>
-         
-        
-      </div>
-     );
- }
-}
+ } 
+
 export default Pproduct;
