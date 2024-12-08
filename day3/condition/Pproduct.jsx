beb852cef1d9f638d8f1dbafb934336da4fc5c60
Nisha <rainishani400@gmail.com>
2024-12-08 15:14:25 +0545

    Add files via upload

--- day3/condition/Pproduct.jsx
+++ day3/condition/Pproduct.jsx
@@ -1,9 +1,10 @@
 import "./Pproduct.css";
 function Pproduct ( {title ,price ,features }){
+   let styles ={backgroundColor:  price > 30000 ? "blue" :""};
  
 
    return(
-      <div className="Pproduct">
+      <div className="Pproduct" style={styles}>
          <h1>{title}</h1>
          <h5> price : {price}</h5>
         { price>30000? <p>"discount of 5%"</p>: <a href="/get discount"></a>}
