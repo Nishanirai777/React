f3aa0b264fd695947d3c014f3c86fe2a1b04468d
Nisha <rainishani400@gmail.com>
2024-12-08 15:13:20 +0545

    Add files via upload

--- day3/Pproduct.jsx
+++ day3/Pproduct.jsx
@@ -1,15 +1,16 @@
 import "./Pproduct.css";
 function Pproduct ( {title ,price ,features }){
+   let styles ={backgroundColor:  price > 30000 ? "blue" :""};
+ 
 
    return(
- <div className="Pproduct">
-    <h1>{title}</h1>
-    <h5> price : {price}</h5>
-    <p>{features.map((feature) => (
-    <li>{feature}</li>
- ))}</p>
-   
- </div>
-);
-}
+      <div className="Pproduct" style={styles}>
+         <h1>{title}</h1>
+         <h5> price : {price}</h5>
+        { price>30000? <p>"discount of 5%"</p>: <a href="/get discount"></a>}
+        
+      </div>
+     );
+ } 
+
 export default Pproduct;
