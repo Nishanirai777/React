c3fe895fd979d66af3b57d92ae6c35aa84fed3bf
Nisha <rainishani400@gmail.com>
2024-12-18 21:15:58 +0545

    Add files via upload

---
+++ part5/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Counter from './Counter';
+import LikeButton from './LikeButton';
+//import Counter from './Counter';
+
+function App() {
+  return (
+    <>
+<p>State React</p>
+  <Counter/>
+    </>
+  );
+}
+
+export default App
